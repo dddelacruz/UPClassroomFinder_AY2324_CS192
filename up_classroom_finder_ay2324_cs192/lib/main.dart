d68b976e2547e0710ffb388dd4fd97c7a248b65d
dddelacruz <dddelacruz7@up.edu.ph>
2024-02-29 21:49:00 +0800

    added floorplanui

--- up_classroom_finder_ay2324_cs192/lib/main.dart
+++ up_classroom_finder_ay2324_cs192/lib/main.dart
@@ -152,6 +152,157 @@ class BookmarksPage extends StatelessWidget{
                         leading: const Icon(Icons.arrow_drop_down),
                         title: Text("AECH"),
                         visualDensity:  const VisualDensity(horizontal: 0, vertical: -4),
+                        onTap: () {
+                          showModalBottomSheet<void>(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return ListView(
+                                children: [
+                                  Container(
+                                      padding: EdgeInsets.all(16),
+                                      height: 600,
+                                      decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(16),
+                                          color: Colors.white),
+                                      child: const Column(
+                                        children: [
+                                          Padding(
+                                            padding: EdgeInsets.only(bottom: 16),
+                                            child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.spaceBetween,
+                                                children: [
+                                                  Text(
+                                                    "AECH",
+                                                    style: TextStyle(
+                                                        fontSize: 20,
+                                                        fontWeight: FontWeight.bold),
+                                                  ),
+                                                  Icon(
+                                                    Icons.bookmark,
+                                                    size: 34,
+                                                    color: Color(0xff800000),
+                                                  )
+                                                ]),
+                                          ), // heading and bookmark row
+                                          Padding(
+                                            padding: EdgeInsets.only(bottom: 8),
+                                            child: Row(
+                                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                              children: [
+                                                CircleAvatar(
+                                                  radius: 16,
+                                                  backgroundColor: Color(0xfff9f9f9),
+                                                  child: Icon(Icons.arrow_back_ios_outlined,
+                                                      size: 18, color: Color(0xff800000)),
+                                                ),
+                                                Text("Floor plan image"),
+                                                CircleAvatar(
+                                                  radius: 16,
+                                                  backgroundColor: Color(0xfff9f9f9),
+                                                  child: Icon(Icons.arrow_forward_ios,
+                                                      size: 18, color: Color(0xff800000)),
+                                                ),
+                                              ],
+                                            ),
+                                          ), // floorplan image row
+                                          Divider(),
+                                          Padding(
+                                            padding: EdgeInsets.only(top: 8, bottom: 8),
+                                            child: Row(
+                                              children: [
+                                                CircleAvatar(
+                                                  radius: 16,
+                                                  backgroundColor: Color(0xffffe9ea),
+                                                  child: Icon(Icons.location_on,
+                                                      size: 18, color: Color(0xffd34343)),
+                                                ),
+                                                Padding(
+                                                  padding: EdgeInsets.only(left: 16),
+                                                  child: Column(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment.start,
+                                                    children: [
+                                                      Text(
+                                                        "Address",
+                                                        style:
+                                                            TextStyle(color: Color(0xff999999)),
+                                                      ),
+                                                      Text("Address Info",
+                                                          style: TextStyle(
+                                                              fontWeight: FontWeight.bold))
+                                                    ],
+                                                  ),
+                                                )
+                                              ],
+                                            ),
+                                          ), // address row
+                                          Padding(
+                                            padding: EdgeInsets.only(bottom: 8),
+                                            child: Row(
+                                              children: [
+                                                CircleAvatar(
+                                                  radius: 16,
+                                                  backgroundColor: Color(0xffffe9ea),
+                                                  child: Icon(Icons.access_time_sharp,
+                                                      size: 18, color: Color(0xffd34343)),
+                                                ),
+                                                Padding(
+                                                  padding: EdgeInsets.only(left: 16),
+                                                  child: Column(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment.start,
+                                                    children: [
+                                                      Text("Opening Hours",
+                                                          style: TextStyle(
+                                                              color: Color(0xff999999))),
+                                                      Text("Opening Hours Info",
+                                                          style: TextStyle(
+                                                              fontWeight: FontWeight.bold))
+                                                    ],
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ), // hours row
+                                          Padding(
+                                            padding: EdgeInsets.only(bottom: 8),
+                                            child: Row(
+                                              children: [
+                                                CircleAvatar(
+                                                  radius: 16,
+                                                  backgroundColor: Color(0xffffe9ea),
+                                                  child: Icon(Icons.emoji_transportation,
+                                                      size: 18, color: Color(0xffd34343)),
+                                                ),
+                                                Padding(
+                                                  padding: EdgeInsets.only(left: 16),
+                                                  child: Column(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment.start,
+                                                    children: [
+                                                      Text(
+                                                        "Transportation",
+                                                        style:
+                                                            TextStyle(color: Color(0xff999999)),
+                                                      ),
+                                                      Text("Transportation Info",
+                                                          style: TextStyle(
+                                                              fontWeight: FontWeight.bold))
+                                                    ],
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ), // transportation row
+                                          Divider(),
+                                        ],
+                                      ))
+                                ],
+                              );
+                            },
+                          );
+                        },
                       ),
                   ],
                 ),
