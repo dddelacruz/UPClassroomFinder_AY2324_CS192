df8331811ea3a8856b5b650d905cd8644466f97b
dddelacruz <dddelacruz7@up.edu.ph>
2024-03-11 13:05:44 +0800

    reorganized floorplanui and bookmarksui code

--- up_classroom_finder_ay2324_cs192/lib/main.dart
+++ up_classroom_finder_ay2324_cs192/lib/main.dart
@@ -124,37 +124,27 @@ class BookmarksPage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Bookmarks',
+                  style: TextStyle(color: Colors.white,
+                        fontWeight: FontWeight.bold,)),
+        backgroundColor: Color(0xff264B30),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 10.0),
+          child: Icon(
+                        Icons.bookmark, 
+                        color: Color(0xff800000),
+                        size: 35,
+                        ),
+        ),
+        titleSpacing: 10,
+      ),
       body: SafeArea(
         child: Column(
           children: [
-            Container(
-              color: const Color(0xff264B30),
-              child: const Padding(
-                padding:  const EdgeInsets.all(15.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Icon(
-                      Icons.bookmark, 
-                      color: Color(0xff800000),
-                      size: 35,
-                      ),
-                    SizedBox(width: 7),
-                    Text(
-                      "Bookmarks",
-                      style: TextStyle(
-                        fontSize: 25,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                  ],
-                ),
-              ),
-              ),
             Expanded(
               child: Padding(
-                padding: const EdgeInsets.only(left: 20, top: 10),
+                padding: const EdgeInsets.only(left: 10, top: 10),
                 child: ListView(
                   children: [
                       ListTile(
@@ -164,163 +154,9 @@ class BookmarksPage extends StatelessWidget{
                         onTap: () {
                           showModalBottomSheet<void>(
                             context: context,
-                            builder: (BuildContext context) {
-                              return ListView(
-                                children: [
-                                  Container(
-                                      padding: EdgeInsets.all(16),
-                                      height: 600,
-                                      decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(16),
-                                          color: Colors.white),
-                                      child: Column(
-                                        children: [
-                                          const Padding(
-                                            padding: EdgeInsets.only(bottom: 16),
-                                            child: Row(
-                                                mainAxisAlignment:
-                                                    MainAxisAlignment.spaceBetween,
-                                                children: [
-                                                  Text(
-                                                    "AECH",
-                                                    style: TextStyle(
-                                                        fontSize: 20,
-                                                        fontWeight: FontWeight.bold),
-                                                  ),
-                                                  Icon(
-                                                    Icons.bookmark,
-                                                    size: 34,
-                                                    color: Color(0xff800000),
-                                                  )
-                                                ]),
-                                          ), // heading and bookmark row
-                                          const Padding(
-                                            padding: EdgeInsets.only(bottom: 8),
-                                            child: Row(
-                                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                              children: [
-                                                CircleAvatar(
-                                                  radius: 16,
-                                                  backgroundColor: Color(0xfff9f9f9),
-                                                  child: Icon(Icons.arrow_back_ios_outlined,
-                                                      size: 18, color: Color(0xff800000)),
-                                                ),
-                                                Text("Floor plan image"),
-                                                CircleAvatar(
-                                                  radius: 16,
-                                                  backgroundColor: Color(0xfff9f9f9),
-                                                  child: Icon(Icons.arrow_forward_ios,
-                                                      size: 18, color: Color(0xff800000)),
-                                                ),
-                                              ],
-                                            ),
-                                          ), // floorplan image row
-                                          const Divider(),
-                                          const Padding(
-                                            padding: EdgeInsets.only(top: 8, bottom: 8),
-                                            child: Row(
-                                              children: [
-                                                CircleAvatar(
-                                                  radius: 16,
-                                                  backgroundColor: Color(0xffffe9ea),
-                                                  child: Icon(Icons.location_on,
-                                                      size: 18, color: Color(0xffd34343)),
-                                                ),
-                                                Padding(
-                                                  padding: EdgeInsets.only(left: 16),
-                                                  child: Column(
-                                                    crossAxisAlignment:
-                                                        CrossAxisAlignment.start,
-                                                    children: [
-                                                      Text(
-                                                        "Address",
-                                                        style:
-                                                            TextStyle(color: Color(0xff999999)),
-                                                      ),
-                                                      Text("Address Info",
-                                                          style: TextStyle(
-                                                              fontWeight: FontWeight.bold))
-                                                    ],
-                                                  ),
-                                                )
-                                              ],
-                                            ),
-                                          ), // address row
-                                          const Padding(
-                                            padding: EdgeInsets.only(bottom: 8),
-                                            child: Row(
-                                              children: [
-                                                CircleAvatar(
-                                                  radius: 16,
-                                                  backgroundColor: Color(0xffffe9ea),
-                                                  child: Icon(Icons.access_time_sharp,
-                                                      size: 18, color: Color(0xffd34343)),
-                                                ),
-                                                Padding(
-                                                  padding: EdgeInsets.only(left: 16),
-                                                  child: Column(
-                                                    crossAxisAlignment:
-                                                        CrossAxisAlignment.start,
-                                                    children: [
-                                                      Text("Opening Hours",
-                                                          style: TextStyle(
-                                                              color: Color(0xff999999))),
-                                                      Text("Opening Hours Info",
-                                                          style: TextStyle(
-                                                              fontWeight: FontWeight.bold))
-                                                    ],
-                                                  ),
-                                                ),
-                                              ],
-                                            ),
-                                          ), // hours row
-                                          const Padding(
-                                            padding: EdgeInsets.only(bottom: 8),
-                                            child: Row(
-                                              children: [
-                                                CircleAvatar(
-                                                  radius: 16,
-                                                  backgroundColor: Color(0xffffe9ea),
-                                                  child: Icon(Icons.emoji_transportation,
-                                                      size: 18, color: Color(0xffd34343)),
-                                                ),
-                                                Padding(
-                                                  padding: EdgeInsets.only(left: 16),
-                                                  child: Column(
-                                                    crossAxisAlignment:
-                                                        CrossAxisAlignment.start,
-                                                    children: [
-                                                      Text(
-                                                        "Transportation",
-                                                        style:
-                                                            TextStyle(color: Color(0xff999999)),
-                                                      ),
-                                                      Text("Transportation Info",
-                                                          style: TextStyle(
-                                                              fontWeight: FontWeight.bold))
-                                                    ],
-                                                  ),
-                                                ),
-                                              ],
-                                            ),
-                                          ), // transportation row
-                                          const Divider(),
-                                          TextButton(
-                                            onPressed: (){
-                                              Navigator.push(
-                                                context,
-                                                MaterialPageRoute(builder: (context) => NotesPage()),
-                                              );
-                                            },
-                                            child: const Text("Notes"),
-                                          ),
-                                        ],
-                                      ))
-                                ],
-                              );
-                            },
+                            builder: (BuildContext context) => const FloorPlanPage(),
                           );
-                        },
+                        }
                       ),
                   ],
                 ),
@@ -421,3 +257,165 @@ class _NotesPage extends State<NotesPage> {
   }
 }
 // *** Notes page state ***
+
+
+class FloorPlanPage extends StatelessWidget{
+  const FloorPlanPage({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return ListView(
+          children: [
+            Container(
+                padding: EdgeInsets.all(16),
+                height: 600,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.white),
+                child: Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 16),
+                      child: Row(
+                          mainAxisAlignment:
+                              MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "AECH",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Icon(
+                              Icons.bookmark,
+                              size: 34,
+                              color: Color(0xff800000),
+                            )
+                          ]),
+                    ), // heading and bookmark row
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 8),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          CircleAvatar(
+                            radius: 16,
+                            backgroundColor: Color(0xfff9f9f9),
+                            child: Icon(Icons.arrow_back_ios_outlined,
+                                size: 18, color: Color(0xff800000)),
+                          ),
+                          Text("Floor plan image"),
+                          CircleAvatar(
+                            radius: 16,
+                            backgroundColor: Color(0xfff9f9f9),
+                            child: Icon(Icons.arrow_forward_ios,
+                                size: 18, color: Color(0xff800000)),
+                          ),
+                        ],
+                      ),
+                    ), // floorplan image row
+                    const Divider(),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 8, bottom: 8),
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 16,
+                            backgroundColor: Color(0xffffe9ea),
+                            child: Icon(Icons.location_on,
+                                size: 18, color: Color(0xffd34343)),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 16),
+                            child: Column(
+                              crossAxisAlignment:
+                                  CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Address",
+                                  style:
+                                      TextStyle(color: Color(0xff999999)),
+                                ),
+                                Text("Address Info",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold))
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ), // address row
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 8),
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 16,
+                            backgroundColor: Color(0xffffe9ea),
+                            child: Icon(Icons.access_time_sharp,
+                                size: 18, color: Color(0xffd34343)),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 16),
+                            child: Column(
+                              crossAxisAlignment:
+                                  CrossAxisAlignment.start,
+                              children: [
+                                Text("Opening Hours",
+                                    style: TextStyle(
+                                        color: Color(0xff999999))),
+                                Text("Opening Hours Info",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold))
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ), // hours row
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 8),
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 16,
+                            backgroundColor: Color(0xffffe9ea),
+                            child: Icon(Icons.emoji_transportation,
+                                size: 18, color: Color(0xffd34343)),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 16),
+                            child: Column(
+                              crossAxisAlignment:
+                                  CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Transportation",
+                                  style:
+                                      TextStyle(color: Color(0xff999999)),
+                                ),
+                                Text("Transportation Info",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold))
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ), // transportation row
+                    const Divider(),
+                    TextButton(
+                      onPressed: (){
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => NotesPage()),
+                        );
+                      },
+                      child: const Text("Notes"),
+                    ),
+                  ],
+                ))
+          ],
+        );
+  }
+}
