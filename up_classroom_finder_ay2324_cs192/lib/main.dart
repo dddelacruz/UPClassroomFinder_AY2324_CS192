de0cd3ab4c4f1527a612001385f11387f9b759f4
ramoncomendadorjr <90377671+ramoncomendadorjr@users.noreply.github.com>
2024-03-14 20:03:39 +0800

    Update main.dart

--- up_classroom_finder_ay2324_cs192/lib/main.dart
+++ up_classroom_finder_ay2324_cs192/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
