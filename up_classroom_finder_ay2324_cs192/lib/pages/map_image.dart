67002f22f6c26bcf3fdbdcf300b98f129e260ff8
Ramon Comendador Jr. <rccomendador@up.edu.ph>
2024-05-03 01:55:49 +0800

    removed appbar in map_image.dart

--- up_classroom_finder_ay2324_cs192/lib/pages/map_image.dart
+++ up_classroom_finder_ay2324_cs192/lib/pages/map_image.dart
@@ -14,41 +14,37 @@ class _MapIMGState extends State<MapIMG> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Maps'),
-        ),
         body: FlutterMap(
-          options: MapOptions(
-            initialCenter: LatLng(14.64860, 121.06855),
-            initialZoom: 18,
-            interactionOptions: InteractionOptions(
-                flags: InteractiveFlag.pinchZoom | InteractiveFlag.drag),
-          ),
-          children: [
-            TileLayer(
-              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-              userAgentPackageName: 'com.example. app',
-            ),
-            MarkerLayer(
-              markers: [
-                Marker(
-                  point: LatLng(14.64866, 121.06866),
-                  width: 80,
-                  height: 80,
-                  child: GestureDetector(
-                    onTap: () {
-                      showModalBottomSheet<void>(
-                        context: context,
-                        builder: (BuildContext context) =>
-                            const FloorPlanPage(),
-                      );
-                    },
-                    child: Icon(Icons.location_pin, size: 50),
-                  ),
-                ),
-              ],
+      options: MapOptions(
+        initialCenter: LatLng(14.64860, 121.06855),
+        initialZoom: 18,
+        interactionOptions: InteractionOptions(
+            flags: InteractiveFlag.pinchZoom | InteractiveFlag.drag),
+      ),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example. app',
+        ),
+        MarkerLayer(
+          markers: [
+            Marker(
+              point: LatLng(14.64866, 121.06866),
+              width: 80,
+              height: 80,
+              child: GestureDetector(
+                onTap: () {
+                  showModalBottomSheet<void>(
+                    context: context,
+                    builder: (BuildContext context) => const FloorPlanPage(),
+                  );
+                },
+                child: Icon(Icons.location_pin, size: 50),
+              ),
             ),
           ],
-        ));
+        ),
+      ],
+    ));
   }
 }
