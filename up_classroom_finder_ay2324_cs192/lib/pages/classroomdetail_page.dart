bdc621be9ab5a9e45b33ba5f4633993790f942ea
dddelacruz <dddelacruz7@up.edu.ph>
2024-05-10 20:16:02 +0800

    made it so that floorplan image generates dynamically

--- up_classroom_finder_ay2324_cs192/lib/pages/classroomdetail_page.dart
+++ up_classroom_finder_ay2324_cs192/lib/pages/classroomdetail_page.dart
@@ -1,75 +1,87 @@
 import 'package:flutter/material.dart';
 
+// hardcoded assets list for locations with floorplan
+final floorplanImgList =[
+  "UP AECH",
+];
+
+
 class ClassroomDetailPage extends StatelessWidget {
   final Map<String, dynamic> upclassroom;
 
-  const ClassroomDetailPage({Key? key, required this.upclassroom})
-      : super(key: key);
+  const ClassroomDetailPage(this.upclassroom, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // classroom name
       appBar: AppBar(
         title: Text(upclassroom['CLASSROOM NUMBER']),
       ),
+    
+      // classroom info
       body: ListView(
         children: [
-          SizedBox(
+          const SizedBox(
             height: 14,
           ),
-          upclassroom['LOCATION'] == "UP AECH"
+    
+          // floorplan
+
+          // check if florplan image exists
+          floorplanImgList.contains(upclassroom['LOCATION'])
+
+              // if file exists, load image
               ? Dialog(
                   child: Image.asset(
-                    "assets/floorplan_AECH.jpg",
+                    "assets/floorplan_${upclassroom['LOCATION']}.jpg",
                     height: 200,
                     width: 200,
                     fit: BoxFit.cover,
                   ),
                 )
-              : Center(child: Text("No floorplan available as of now...")),
-          SizedBox(
+    
+              // file does not exist
+              : const Center(child: Text("No floorplan available as of now...")),
+          const SizedBox(
             height: 14,
           ),
+    
+          // address
           ListTile(
-            leading: CircleAvatar(
+            leading: const CircleAvatar(
               backgroundColor: Color(0xffffe9ea),
               child:
                   Icon(Icons.location_on, size: 18, color: Color(0xffd34343)),
             ),
-            title: Text("Address"),
+            title: const Text("Address"),
             subtitle: Text(upclassroom['LOCATION']),
           ),
+    
+          // opening hours
           ListTile(
-            leading: CircleAvatar(
+            leading: const CircleAvatar(
               backgroundColor: Color(0xffffe9ea),
               child: Icon(Icons.access_time_sharp,
                   size: 18, color: Color(0xffd34343)),
             ),
-            title: Text("Opening Hours"),
+            title: const Text("Opening Hours"),
             subtitle: Text(upclassroom['OPENING HOURS']),
           ),
+    
+          // transportation
           ListTile(
-            leading: CircleAvatar(
+            leading: const CircleAvatar(
               backgroundColor: Color(0xffffe9ea),
               child: Icon(Icons.emoji_transportation,
                   size: 18, color: Color(0xffd34343)),
             ),
-            title: Text("Transportation"),
+            title: const Text("Transportation"),
             subtitle: Text(upclassroom['TRANSPORTATION']),
           ),
-          // TextButton(
-          //   onPressed: () {
-          //     Navigator.push(
-          //       context,
-          //       MaterialPageRoute(
-          //         builder: (context) => FloorPlanPage(),
-          //       ),
-          //     );
-          //   },
-          //   child: Text("View Floor Plan"),
-          // ),
         ],
       ),
+    
     );
   }
 }
