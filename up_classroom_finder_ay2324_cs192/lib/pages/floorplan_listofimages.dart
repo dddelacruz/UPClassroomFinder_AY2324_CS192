bd85fe84a9271350cbe2c32b0deba538b8bc9cee
Ramon Comendador Jr. <rccomendador@up.edu.ph>
2024-05-14 22:25:16 +0800

    added new DMMME floorplan images

--- up_classroom_finder_ay2324_cs192/lib/pages/floorplan_listofimages.dart
+++ up_classroom_finder_ay2324_cs192/lib/pages/floorplan_listofimages.dart
@@ -6,7 +6,10 @@ class FloorPlanImagePageList extends StatelessWidget {
 
   final List<String> floorplanImagePathList = [
     'assets/floorplan_AECH_2ND.jpg',
-    'assets/floorplan_DMMME_1ST.jpg',
+    'assets/floorplan_DMME_LGF.jpg',
+    'assets/floorplan_DMMME_2F.jpg',
+    'assets/floorplan_DMMME_3F.jpg',
+    'assets/floorplan_DMMME_UGF.jpg',
     'assets/floorplan_EEEI_1ST.jpg',
     'assets/floorplan_EEEI_2ND.jpg',
     'assets/floorplan_NIGS_2ND.jpg',
@@ -22,18 +25,24 @@ class FloorPlanImagePageList extends StatelessWidget {
     switch (buildingName + floorNumber) {
       case 'AECH2ND':
         return Center(child: Image.asset(floorplanImagePathList[0]));
-      case 'DMMME1ST':
+      case 'DMMMELGF':
         return Center(child: Image.asset(floorplanImagePathList[1]));
-      case 'EEEI1ST':
+      case 'DMMME2F':
         return Center(child: Image.asset(floorplanImagePathList[2]));
-      case 'EEEI2ND':
+      case 'DMMME3F':
         return Center(child: Image.asset(floorplanImagePathList[3]));
-      case 'NIGS2ND':
+      case 'DMMMEUGF':
         return Center(child: Image.asset(floorplanImagePathList[4]));
-      case 'NIGSBASEMENT':
+      case 'EEEI1ST':
         return Center(child: Image.asset(floorplanImagePathList[5]));
-      case 'NIGSGROUND':
+      case 'EEEI2ND':
         return Center(child: Image.asset(floorplanImagePathList[6]));
+      case 'NIGS2ND':
+        return Center(child: Image.asset(floorplanImagePathList[7]));
+      case 'NIGSBASEMENT':
+        return Center(child: Image.asset(floorplanImagePathList[8]));
+      case 'NIGSGROUND':
+        return Center(child: Image.asset(floorplanImagePathList[9]));
       // INSERT OTHER FLOORPLANS
       default:
         return Center(child: Text("No floorplan available"));
